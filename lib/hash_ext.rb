153c7ec1b18a95510df8751db2b951e7f060a564
Gabriel Defagot <gdefagot@keepcon.com>
2016-09-29 11:55:12 -0300

    Added normalized hash. Indefferent is sublass of Normalized

--- lib/hash_ext.rb
+++ lib/hash_ext.rb
@@ -2,6 +2,7 @@ require 'forwardable'
 
 require_relative 'hash_ext/version'
 
+require_relative 'hash_ext/normalized'
 require_relative 'hash_ext/builder'
 require_relative 'hash_ext/indifferent'
 require_relative 'hash_ext/accessible'
---
+++ lib/hash_ext/normalized.rb
@@ -0,0 +1,70 @@
+class Hash
+  class Normalized < Hash
+
+    def self.subclass(&block)
+      Class.new(self) do
+        define_method :initialize do |hash={}|
+          super hash, &block
+        end
+      end
+    end
+
+    def initialize(hash={}, &block)
+      @normalization_block = block
+      update hash
+    end
+
+    def [](key)
+      super normalize_key(key)
+    end
+
+    def []=(key, value)
+      super normalize_key(key), normalize_value(value)
+    end
+    alias_method :store, :[]=
+
+    def delete(key)
+      super normalize_key(key)
+    end
+
+    def update(hash)
+      hash.each do |key, value|
+        store normalize_key(key), normalize_value(value)
+      end
+      self
+    end
+    alias_method :merge!, :update
+
+    def merge(hash)
+      dup.update hash
+    end
+
+    def key?(key)
+      super normalize_key(key)
+    end
+    alias_method :include?, :key?
+    alias_method :has_key?, :key?
+    alias_method :member?, :key?
+
+    def fetch(key, *args, &block)
+      super normalize_key(key), *args, &block
+    end
+
+    private
+
+    def normalize_key(key)
+      @normalization_block.call key
+    end
+
+    def normalize_value(value)
+      if value.kind_of? Hash
+        self.class.new value
+      elsif value.kind_of? Array
+        value.map { |v| normalize_value v }
+      else
+        value
+      end
+    end
+
+  end
+end
