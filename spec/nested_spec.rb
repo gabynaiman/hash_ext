7b308baf229c230cb662e13ee4ad3c30cb63cb10
Gabriel Naiman <gnaiman@keepcon.com>
2018-01-31 11:42:57 -0300

    Complete coverage

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -7,13 +7,21 @@ describe Hash::Nested do
 
     hash[:undefined].must_equal Hash.new
   end
+
+  it 'Keept assigned value' do
+    hash = Hash::Nested.new
+
+    hash[:key] = 'value'
+
+    hash[:key].must_equal 'value'
+  end
   
   it 'Allow set nested hash levels' do
     hash = Hash::Nested.new
 
     hash[:level_1][:level_2][:level_3] = 'test'
 
-    hash.must_equal({ level_1: { level_2: { level_3: "test" } } })
+    hash.must_equal level_1: {level_2: {level_3: 'test'}}
   end
 
 end
