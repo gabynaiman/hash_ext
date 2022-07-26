24f9000da6655de4a5a1f4ce105c84201641f933
Gabriel Naiman <gnaiman@keepcon.com>
2022-07-26 11:03:25 -0300

    Replaced coveralls dependency with simplecov-lcov

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -1,5 +1,5 @@
 require 'simplecov'
-require 'coveralls'
+require 'simplecov-lcov'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [SimpleCov::Formatter::HTMLFormatter, SimpleCov::Formatter::LcovFormatter]
 SimpleCov.start
